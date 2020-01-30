b7a25f07a495617adf0c7ddc8405cfaa1d6978d6
Juyeong Lee <ljy3795@gmail.com>
2020-01-30 11:44:25 +0900

    fix docker_run file

--- project/mls_bootcamp/client/docker_run.sh
+++ project/mls_bootcamp/client/docker_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 3000:3000 --name go-mls-bootcamp-react mls-bootcamp-react:latest
+docker run -d -p 3000:3000 --rm --name go-mls-bootcamp-react mls-bootcamp-react:latest

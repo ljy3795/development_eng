f14b7433b7b3b0f7c66896726dd17006105cc0b3
Juyeong Lee <ljy3795@gmail.com>
2020-01-30 16:56:26 +0900

    reset

--- project/mls_bootcamp/client/docker_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d -p 3000:3000 --rm --name go-mls-bootcamp-react mls-bootcamp-react:latest
--- project/mls_bootcamp/server/docker_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d -p 8080:8080 --env-file .env --rm --name go-mls-bootcamp-server mls-bootcamp-server:latest
